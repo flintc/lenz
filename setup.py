8a7503eca36c9591ee649009be0eec588a55a7f5
flintc <35920467+flintc@users.noreply.github.com>
2019-01-29 09:28:08 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lenz',
+      version='0.1',
+      description='A functional lens library for python',
+      url='http://github.com/flintc/lenz',
+      license='MIT',
+      packages=['lenz'],
+      zip_safe=False)
